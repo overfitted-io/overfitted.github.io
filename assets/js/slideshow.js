cda712d356d32564ba4f5b6fdc550443ec1ca0ae
Dan Sporici <dansporici@gmail.com>
2022-04-21 17:32:39 +0300

    added slideshow, demos

---
+++ assets/js/slideshow.js
@@ -0,0 +1,27 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "block";
+}
+
+function changeSlides() {
+    showSlides(slideIndex += 1)
+
+}
+
+setInterval(changeSlides, 5000);
